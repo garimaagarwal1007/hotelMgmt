f40870d9dbcaf5bd16ddcea55c5c2f48d41784cf
unknown <garima.agarwal1007@gmail.com>
2021-07-28 01:31:07 +0530

    db conn

--- DBConnection/db_conn.py
+++ DBConnection/db_conn.py
@@ -1,2 +1,23 @@
 import pymongo
+from pymongo import MongoClient
+
+from utils.hotelConfig import Config
+
+CONFIG = Config.get_config()
+# for sect in CONFIG.sections():
+#     for k, v in CONFIG.items(sect):
+#         print(k, v)
+
+
+print(CONFIG.get('DB', 'conn_string'))
+conn_str = CONFIG.get('DB', 'conn_string')
+
+client = MongoClient(conn_str)
+db_name = client["HotelMgmt"]
+collection_name = db_name["hotels"]
+
+all_item_details = collection_name.find()
+for item in all_item_details:
+    #     # This does not give a very readable output
+    print(item)
 
--- utils/hotelConfig.py
+++ utils/hotelConfig.py
@@ -1,20 +1,14 @@
 import configparser, sys, os
 
-
-def getFilePath(filename):
-    basepath = "/cfg"
-    dirname = os.path.dirname(basepath)
-    print("directory - ", dirname)
-    return os.path.join(basepath, filename)
+PATH = "D:\hotelMgmt\cfg\config.ini"
 
 
-def readConfigFile(filename):
-    parser = configparser.ConfigParser()
-    parser.read(getFilePath(filename))
-    return parser
+class Config:
+    _instance = None
 
-
-parser = readConfigFile("config.ini")
-for sect in parser.sections():
-    for k, v in parser.items(sect):
-        print(k, v)
+    @classmethod
+    def get_config(cls):
+        if cls._instance is None:
+            cls._instance = configparser.ConfigParser()
+            cls._instance.read(PATH)
+        return cls._instance
