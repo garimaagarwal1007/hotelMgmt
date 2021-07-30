78d40e4cbc29b194b00b2277ec1b4491287a0a89
unknown <garima.agarwal1007@gmail.com>
2021-07-31 00:15:38 +0530

    folder creations

---
+++ DAO/db_util.py
@@ -0,0 +1,21 @@
+class Hotel:
+    def __init__(self):
+        self.hotel_id = ""
+        self.name = ""
+
+    def get_insert_values(self):
+        # help create insert query for hotel table
+        pass
+
+    def get_header(self):
+        # insert
+        pass
+
+    def update(self):
+        pass
+
+
+result = (1234, "TAjAkshay")
+hotel = Hotel()
+hotel.hotel_id = result[0]
+hotel.name = result[0]
--- utils/hotelUtils.py
+++ utils/hotelUtils.py
@@ -2,7 +2,7 @@ class HotelUtils:
     def __init__(self):
         pass
 
-    @staticmethod
-    def get_auto_incremented(field_name):
-        sequence = db.counters.find_one_and_update({"_id": "user_id"}, {"$inc": {"sequence_value": 1}},
-                                                   new=True)
+    # @staticmethod
+    # def get_auto_incremented(field_name):
+    #     sequence = db.counters.find_one_and_update({"_id": "user_id"}, {"$inc": {"sequence_value": 1}},
+    #                                                new=True)
