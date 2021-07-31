028091c415b5896f2414d3a1f647fa8d805b6afd
nevil.panchal <nevil.panchal@visiblealpha.com>
2021-07-31 20:01:13 +0530

    Model folder added

---
+++ DAO/Models/Hotels.py
@@ -0,0 +1,12 @@
+
+class Hotels:
+    def __init__(self,hotel_id=0,hotel_name='',is_active=False,check_in='',check_out=''):
+        self.hotel_id=hotel_id
+        self.hotel_name= hotel_name
+        self.is_active=is_active
+        self.check_in=check_in
+        self.check_out=check_out
+
+
+    def get_hotel_by_id(self,hotel_id):
+        pass
