20773bc3896832a357ffd3507fd0298f91cca2d9
unknown <garima.agarwal1007@gmail.com>
2021-07-31 23:04:27 +0530

    model example

--- DAO/Models/Hotels.py
+++ DAO/Models/Hotels.py
@@ -1,12 +1,26 @@
+import Constants
+
 
 class Hotels:
-    def __init__(self,hotel_id=0,hotel_name='',is_active=False,check_in='',check_out=''):
-        self.hotel_id=hotel_id
-        self.hotel_name= hotel_name
-        self.is_active=is_active
-        self.check_in=check_in
-        self.check_out=check_out
+    def __init__(self, hotel_id, hotel_name, desc, amenities, policies, address, check_in='12:00 PM',
+                 check_out='11:00 AM',
+                 is_active=1):
+        self.hotel_id = hotel_id
+        self.hotel_name = hotel_name
+        self.hotel_desc = desc
+        self.amenities = amenities
+        self.policies = policies
+        self.address = address
+        self.is_active = is_active
+        self.check_in = check_in
+        self.check_out = check_out
+
+    @staticmethod
+    def get_insert_header_for_hotel():
+        return Constants.HOTEL_INSERT_HEADER
+
+    def get_values(self):
+        val = f' ("{self.hotel_name}", "{self.hotel_desc}", {self.is_active}, "{self.check_in}", "{self.check_out}")'
+        return val
 
 
-    def get_hotel_by_id(self,hotel_id):
-        pass
---
+++ DAO/db_conn.py
@@ -0,0 +1,38 @@
+from utils.hotelConfig import Config
+import pymysql
+
+config = Config.get_config()
+
+
+class DAO(object):
+    def __init__(self, attribute="Database"):
+        conn_text = config.get(attribute, 'conn_string')
+        self.conn_string = conn_text.split(",")
+        self.db = attribute
+
+    def execute(self, query, conn_str=None):
+        cnxn = None
+        rows = []
+        if conn_str is None:
+            conn_str = self.conn_string
+        else:
+            conn_str = conn_str.split(",")
+        try:
+            cnxn = pymysql.connect(user=conn_str[1], passwd=conn_str[2], host=conn_str[0],
+                                   database=conn_str[3], autocommit=False)
+            cursor = cnxn.cursor()
+            cursor.execute(query.encode('utf-8'))
+            cnxn.commit()
+            rows = cursor.fetchall()
+        except Exception as e:
+            if cnxn:
+                cnxn.rollback()
+            raise e
+        finally:
+            if cnxn:
+                cnxn.close()
+        return rows
+
+    def execute_non_query(self):
+        pass
+
