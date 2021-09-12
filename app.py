34c8ae47cd77d7b86a9ed5644e08520f89f36577
Akshay Kurichh <akshay.kurichh@mindbodyonline.com>
2021-09-12 18:17:02 +0530

    FastApi setup

--- app.py
+++ app.py
@@ -1,45 +1,11 @@
-from flask import Flask
-from flask import Blueprint
-from flask_swagger_ui import get_swaggerui_blueprint
-from flask_restplus import Api, Namespace, Resource
-from werkzeug.utils import cached_property
-#from flask_restplus import Api
-from entities.RoomTypeInfo import room_type_info
-from entities.bedtype import bed_type
-from entities.roominfo import room_info
-from entities.offerdetails import offer_details
-from entities.currencymaster import currency_master
-from entities.hoteltoroomtypemapper import hotel_roomtype_mapper
-from entities.bookinghistory import booking_history
+from entities.offerdetails import offer_router
+from fastapi import FastAPI
 
-app = Flask(__name__)
-
-# ### swagger specific ###
-# SWAGGER_URL = '/swagger'
-# API_URL = '/static/swagger.json'
-# SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-#     SWAGGER_URL,
-#     API_URL,
-#     config={
-#         'app_name': "Seans-Python-Flask-REST-Boilerplate"
-#     }
-# )
-# app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-#blueprint = Blueprint('api', __name__)
-# api=Api(room_info,ui=True);
-
-app.register_blueprint(room_type_info)
-app.register_blueprint(bed_type)
-app.register_blueprint(room_info)
-app.register_blueprint(offer_details)
-app.register_blueprint(currency_master)
-app.register_blueprint(hotel_roomtype_mapper)
-app.register_blueprint(booking_history)
-
-@app.route('/')
-def home():
-    return "hello";
-
+app = FastAPI(
+    title="Hotel Management API"
+)
+app.include_router(offer_router)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
--- entities/offerdetails.py
+++ entities/offerdetails.py
@@ -1,20 +1,25 @@
 from DAO.db_util import insert
 from DAO.db_conn import DAO
 from flask import Blueprint,request
+from fastapi import FastAPI,APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
-offer_details = Blueprint('offerdetails', __name__)
+class Offer(BaseModel):
+    offer_desc: str
+    is_percent: Optional[int]=0
+    offer_discount:int
+
+offer_router=APIRouter()
+@offer_router.post("/add_offer")
+async def add_offer(data:Offer):
+    query=insert(data)
+    dao=DAO()
+    result=dao.execute_non_execute(query)
+    return result
 
 # class OfferDetails:
 #     def __init__(self):
 #         self.offer_desc="Paytm offer"
 #         self.is_percent=1
 #         self.offer_discount=10
-
-@offer_details.route('/insertoffer',methods=['POST'])
-def insert_offer():
-         data = request.data
-         query=insert(data)
-         dao=DAO()
-         result=dao.execute_non_execute(query)
-         print(result)
-
